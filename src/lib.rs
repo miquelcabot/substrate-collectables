1479cc615d4e120a4405d0db5993dd95c4bf513e
miquelcabot <miquel.cabot@gmail.com>
2025-01-14 17:28:09 +0100

    refactor code formatting and improve indentation in implementation files

--- src/lib.rs
+++ src/lib.rs
@@ -8,34 +8,34 @@ pub use pallet::*;
 
 #[frame::pallet(dev_mode)]
 pub mod pallet {
-	use super::*;
+    use super::*;
 
-	#[pallet::pallet]
-	pub struct Pallet<T>(core::marker::PhantomData<T>);
+    #[pallet::pallet]
+    pub struct Pallet<T>(core::marker::PhantomData<T>);
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-	}
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    }
 
-	#[pallet::storage]
-	pub(super) type CountForKitties<T: Config> = StorageValue<Value = u32>;
+    #[pallet::storage]
+    pub(super) type CountForKitties<T: Config> = StorageValue<Value = u32>;
 
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		Created { owner: T::AccountId },
-	}
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        Created { owner: T::AccountId },
+    }
 
-	#[pallet::error]
-	pub enum Error<T> {}
+    #[pallet::error]
+    pub enum Error<T> {}
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		pub fn create_kitty(origin: OriginFor<T>) -> DispatchResult {
-			let who = ensure_signed(origin)?;
-			Self::mint(who)?;
-			Ok(())
-		}
-	}
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        pub fn create_kitty(origin: OriginFor<T>) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+            Self::mint(who)?;
+            Ok(())
+        }
+    }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -31,55 +31,55 @@ const BOB: u64 = 2;
 
 #[runtime]
 mod runtime {
-	#[runtime::derive(
-		RuntimeCall,
-		RuntimeEvent,
-		RuntimeError,
-		RuntimeOrigin,
-		RuntimeTask,
-		RuntimeHoldReason,
-		RuntimeFreezeReason
-	)]
-	#[runtime::runtime]
-	/// The "test runtime" that represents the state transition function for our blockchain.
-	///
-	/// The runtime is composed of individual modules called "pallets", which you find see below.
-	/// Each pallet has its own logic and storage, all of which can be combined together.
-	pub struct TestRuntime;
+    #[runtime::derive(
+        RuntimeCall,
+        RuntimeEvent,
+        RuntimeError,
+        RuntimeOrigin,
+        RuntimeTask,
+        RuntimeHoldReason,
+        RuntimeFreezeReason
+    )]
+    #[runtime::runtime]
+    /// The "test runtime" that represents the state transition function for our blockchain.
+    ///
+    /// The runtime is composed of individual modules called "pallets", which you find see below.
+    /// Each pallet has its own logic and storage, all of which can be combined together.
+    pub struct TestRuntime;
 
-	/// System: Mandatory system pallet that should always be included in a FRAME runtime.
-	#[runtime::pallet_index(0)]
-	pub type System = frame_system::Pallet<TestRuntime>;
+    /// System: Mandatory system pallet that should always be included in a FRAME runtime.
+    #[runtime::pallet_index(0)]
+    pub type System = frame_system::Pallet<TestRuntime>;
 
-	/// PalletBalances: Manages your blockchain's native currency. (i.e. DOT on Polkadot)
-	#[runtime::pallet_index(1)]
-	pub type PalletBalances = pallet_balances::Pallet<TestRuntime>;
+    /// PalletBalances: Manages your blockchain's native currency. (i.e. DOT on Polkadot)
+    #[runtime::pallet_index(1)]
+    pub type PalletBalances = pallet_balances::Pallet<TestRuntime>;
 
-	/// PalletKitties: The pallet you are building in this tutorial!
-	#[runtime::pallet_index(2)]
-	pub type PalletKitties = pallet_kitties::Pallet<TestRuntime>;
+    /// PalletKitties: The pallet you are building in this tutorial!
+    #[runtime::pallet_index(2)]
+    pub type PalletKitties = pallet_kitties::Pallet<TestRuntime>;
 }
 
 // Normally `System` would have many more configurations, but you can see that we use some macro
 // magic to automatically configure most of the pallet for a "default test configuration".
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
 impl frame_system::Config for TestRuntime {
-	type Block = Block;
-	type AccountData = pallet_balances::AccountData<Balance>;
+    type Block = Block;
+    type AccountData = pallet_balances::AccountData<Balance>;
 }
 
 // Normally `pallet_balances` would have many more configurations, but you can see that we use some
 // macro magic to automatically configure most of the pallet for a "default test configuration".
 #[derive_impl(pallet_balances::config_preludes::TestDefaultConfig)]
 impl pallet_balances::Config for TestRuntime {
-	type AccountStore = System;
-	type Balance = Balance;
+    type AccountStore = System;
+    type Balance = Balance;
 }
 
 // This is the configuration of our Pallet! If you make changes to the pallet's `trait Config`, you
 // will also need to update this configuration to represent that.
 impl pallet_kitties::Config for TestRuntime {
-	type RuntimeEvent = RuntimeEvent;
+    type RuntimeEvent = RuntimeEvent;
 }
 
 // We need to run most of our tests using this function: `new_test_ext().execute_with(|| { ... });`
@@ -87,29 +87,29 @@ impl pallet_kitties::Config for TestRuntime {
 // If you forget to include this and try to access your Pallet storage, you will get an error like:
 // "`get_version_1` called outside of an Externalities-provided environment."
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	frame_system::GenesisConfig::<TestRuntime>::default()
-		.build_storage()
-		.unwrap()
-		.into()
+    frame_system::GenesisConfig::<TestRuntime>::default()
+        .build_storage()
+        .unwrap()
+        .into()
 }
 
 #[test]
 fn starting_template_is_sane() {
-	new_test_ext().execute_with(|| {
-		let event = Event::<TestRuntime>::Created { owner: ALICE };
-		let _runtime_event: RuntimeEvent = event.into();
-		let _call = Call::<TestRuntime>::create_kitty {};
-		let result = PalletKitties::create_kitty(RuntimeOrigin::signed(BOB));
-		assert_ok!(result);
-	});
+    new_test_ext().execute_with(|| {
+        let event = Event::<TestRuntime>::Created { owner: ALICE };
+        let _runtime_event: RuntimeEvent = event.into();
+        let _call = Call::<TestRuntime>::create_kitty {};
+        let result = PalletKitties::create_kitty(RuntimeOrigin::signed(BOB));
+        assert_ok!(result);
+    });
 }
 
 #[test]
 fn system_and_balances_work() {
-	// This test will just sanity check that we can access `System` and `PalletBalances`.
-	new_test_ext().execute_with(|| {
-		// We often need to add some balance to a user to test features which needs tokens.
-		assert_ok!(PalletBalances::mint_into(&ALICE, 100));
-		assert_ok!(PalletBalances::mint_into(&BOB, 100));
-	});
+    // This test will just sanity check that we can access `System` and `PalletBalances`.
+    new_test_ext().execute_with(|| {
+        // We often need to add some balance to a user to test features which needs tokens.
+        assert_ok!(PalletBalances::mint_into(&ALICE, 100));
+        assert_ok!(PalletBalances::mint_into(&BOB, 100));
+    });
 }
