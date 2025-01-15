26c351c5da1ba65d8433b03d19b6b1ff7fa77083
miquelcabot <miquel.cabot@gmail.com>
2025-01-15 15:44:34 +0100

    add NativeBalance trait to Config for balance inspection and mutation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod impls;
 mod tests;
 
 use frame::prelude::*;
+use frame::traits::fungible::Inspect;
+use frame::traits::fungible::Mutate;
 pub use pallet::*;
 
 #[frame::pallet(dev_mode)]
@@ -16,6 +18,8 @@ pub mod pallet {
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+        type NativeBalance: Inspect<Self::AccountId> + Mutate<Self::AccountId>;
     }
 
     #[derive(Encode, Decode, MaxEncodedLen, TypeInfo)]
